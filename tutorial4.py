32b6e8adcae889dd7946e81729984c7e76e2b1d0
rvngrd <rrvngrdd@gmail.com>
2023-03-02 17:25:57 +0330

    finished part 4

--- tutorial4.py
+++ tutorial4.py
@@ -1,3 +1,18 @@
 """
-1:
+1:Arithmatic operators
+2:Relational operators >, <, >=, <=, ==, !=
+3:Logical operators and, not, or
+4:Bitwise operators &, |, ~(not), ^(xor), >>(right shift), <<(left shift)
+5:Assignment operators =, +=, -= and more
+6:Special operators is, is not, in, not in
 """
+x = 7 / 2       # Division float
+print(x)
+y = 7 // 2      # Division and floor of ir
+print(y)
+z = 10 % 3      # Modulus
+print(z)
+m = True        # Boolean
+print(not m)
+n = 2
+print(n << 1)
