06491ab158021d1922619470c7e6164f3cffd531
rvngrd <rrvngrdd@gmail.com>
2023-03-02 14:00:22 +0330

    finished first tutorial

---
+++ tutorial1.py
@@ -0,0 +1,11 @@
+"""
+1:How to install python from https://www.python.org/
+2:Using python idle
+"""
+x = 25
+print('ur number is {}'.format(x))
+# printing number with two decimal places
+print('ur number is %10.2f' % x)
+y = 2.154327
+format_float = "{:.2f}".format(y)
+print(format_float)
