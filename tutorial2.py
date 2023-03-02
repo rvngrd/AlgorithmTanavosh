043c418cad8c6d3ab73af29d347f005c4f4cba8a
rvngrd <rrvngrdd@gmail.com>
2023-03-02 14:22:06 +0330

    finished second tutorial

---
+++ tutorial2.py
@@ -0,0 +1,14 @@
+"""
+1:Using geeksforgeeks ide : https://ide.geeksforgeeks.org/online-python-compiler
+2:Using math module
+"""
+from math import ceil, floor, sqrt, log2
+
+x = 456.70
+print('x is', x)
+print('type of x :', type(x))
+print('ceil of x :', ceil(x))
+print('floor of x :', floor(x))
+print('sqrt of x :', sqrt(x))
+print('log2 of x :', log2(x))
+print('%.2f' % x)
